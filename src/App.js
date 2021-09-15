a2fe771a7b256e5ebc39843aae236e419ac07e65
Ngoc Long <ngoclongg12@gmail.com>
2021-09-15 15:36:16 +0700

    add

--- src/App.js
+++ src/App.js
@@ -17,12 +17,22 @@ function App() {
     {name: "Ngọc", age:2},
     {name: "Long", age:3},
   ]);
+  
+  const onToChange = ( event ) => {
+    console.log( event );
+  
+    setPerson([
+      {name: event.target.value, age:4},
+      {name: event.target.value, age:5},
+      {name: event.target.value, age:6},
+    ])
+  }
 
   return (
     <div className="App">
       <h1>ReactJS this is the first</h1>
 
-      <Person name={persons[0].name} age={Math.random} />
+      <Person changed={onToChange} click={change} name={persons[0].name} age={persons[0].age} />
       <Person name={persons[1].name} age={Math.random} />
       <Person name={persons[2].name} age={Math.random} />
       <button onClick={() => change()}>change</button>
--- src/Components/Person.js
+++ src/Components/Person.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Person = (props) => {
-    return <p>Phạm Ngọc Long {props.name} years old {props.age}</p>
+    return (
+        <div>
+            <p onClick={props.click}>Phạm Ngọc Long {props.name} years old {props.age}</p>
+            <input type="text" onChange={props.changed} ></input>
+        </div>
+
+    )
+
 }
 
 export default Person;
